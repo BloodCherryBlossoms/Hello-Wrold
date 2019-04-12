c99b20e96dd852d1a84ebce8b9cb5e6ba4827d57
your name <your email>
2019-04-12 09:04:27 +0800

    Hello Wrold

---
+++ Assets/Input_my.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_my : MonoBehaviour
+{
+
+    private string names;
+    public Rigidbody rd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        names = "Holle wrold";
+        Debug.Log(names);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWDown = Input.GetKeyDown(KeyCode.S);
+        rd = gameObject.GetComponent<Rigidbody>();
+        rd.AddForce(0, 0, 5);
+        Debug.Log("123");
+    }
+}
